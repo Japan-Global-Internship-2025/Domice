efae334eed6dd691d888b26e3d9d359267217016
minjae1025 <kjt081025@gmail.com>
2025-11-29 23:18:47 +0900

    temp

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const HeaderContainer = styled.div`
+    
+`;
+
+const Logo = styled.img`
+    
+`;
+
+export default function Header() {
+    return (
+        <HeaderContainer>
+            <Logo src="../" />
+        </HeaderContainer>
+    );
+}
