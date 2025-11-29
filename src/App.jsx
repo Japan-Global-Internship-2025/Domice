fa566a80c05c4496c01506ab2b452dc034365d44
minjae1025 <kjt081025@gmail.com>
2025-11-29 22:05:52 +0900

    app.jsx 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import './App.css'
+import { BrowserView, MobileView } from 'react-device-detect'
+import { Route, Routes } from 'react-router-dom'
 
 function App() {
 
   return (
     <>
-      
+      <BrowserView>
+        <h1>PC에서는 제공되지 않습니다.</h1>
+      </BrowserView>
+      <MobileView>
+        <Routes>
+        </Routes>
+      </MobileView>
     </>
   )
 }
